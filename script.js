b187d96f38212fca4231cca6f0ec600cf6afc0ef
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-03-31 13:41:10 -0700

    added pause functionlity! yay

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
 $(document).ready(function() {
 
-$('#form_new_entry').submit(function(event){
-	event.preventDefault();
+// var todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+// for(var i = 0; i < todos.length; i++) {
+// 	addToDo(Todos[i]);
+// }
+
+//$('ul').on('click', 'input:checkbox', strikethrough);
+
+
+/*--- CREATE EVENT LISTERS ---*/
+$('#form_new_entry').submit(addEntry);
+
+$('#form_delete').submit(removeEntry);
+
+$('#form_reset').submit(resetList);
+// function strikethrough(e){
+// 	console.log(e);
+// 	e.preventDefault();
+// 	$(this).next().style.textDecoration('strikethrough');
+// }
+
+});
+
+var addEntry = function(e) {
+	e.preventDefault();
+
 	var str = $('input:text').val();
 	if(str === ''){
 		return;
 	}
 
-	$('ul').append('<li><input class="class_check" type="checkbox"></input>' + str + '</li>');
-	$('input:text').val('');
-});
+	$('ul').sortable();
 
-$('#form_delete').submit(function(event){
+	// create a list item
+
+	// create a delete button
+	var deleteButton = $('<div class="class_button"></div>');
+	var editButton = $('<div class="class_button"</div>');
+	deleteButton.click(mark);
+
+	var listItem = $('<li></li>');
+	$('ul').append(listItem);
+	listItem.append(deleteButton);
+	listItem.append(editButton);
+	listItem.append(str);
+
+	$('input:text').val('');
+}
+
+var removeEntry = function(e) {
 	event.preventDefault();
 
-	$(':checked').parent().remove();
+	$('.class_strike').remove();
+}
 
-});
-
-$('#form_reset').submit(function(event){
+var resetList = function(e) {
 	event.preventDefault();
 
 	$('li').remove();
-});
+}
 
-});
+var mark = function(e) {
+	$(this).parent().toggleClass('class_strike');
+}
